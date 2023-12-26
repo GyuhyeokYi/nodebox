a47feb0b49b4062b9d34e2158160421cc28044f0
gyuhyeok <gyuhyeok@gnifrix.com>
2023-12-26 11:22:44 +0900

    UI as tree 예제 추가

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React, { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./styles.css"
 
-import App from "./Chapter01"
+// import App from "./Chapter01"
+import App from './ui_as_tree/App'
 
 const root = createRoot(document.getElementById("root"))
 root.render(
